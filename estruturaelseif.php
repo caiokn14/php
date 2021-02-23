dc6a49b5450509e0754c7ca8877da6daad9724d9
Caio <caiokn_14@hotmail.com>
2021-02-22 22:46:26 -0300

    if, else, elseif, switch e while

---
+++ estruturaelseif.php
@@ -0,0 +1,19 @@
+<?php
+$prova1 = 7;
+$prova2 = 5;
+
+$nota = ($prova1 + $prova2) / 2;
+
+if ($nota < 3) {
+    echo $desempenho = "PÉSSIMO ". $nota;
+}elseif ($nota < 5) {
+    echo $desempenho = "RUIM ". $nota;
+}elseif ($nota < 7) {
+    echo $desempenho = "MÉDIO ". $nota;
+}elseif ($nota < 9) {
+    echo $desempenho = "BOM ". $nota;
+}else{
+    echo $desempenho = "EXCELENTE ". $nota;
+}
+
+?>
