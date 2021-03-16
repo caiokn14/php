7bcb67ec95264a29f6f0fdf4e26476debd03d270
Caio <caiokn_14@hotmail.com>
2021-03-16 00:02:19 -0300

    organizado em pastas

--- array/arrays.php
+++ array/arrays.php
@@ -3,13 +3,12 @@
     // echo "Eu gosto de $carros[0], $carros[1] e $carros[2]";
     $tamanho = count($carros);
     for($x = 0; $x < $tamanho; $x++){
-        echo "$carros[$x] <br>";
+        //echo "$carros[$x] <br>";
     }
 
-    $laticinios=array("queijo"=>10.0,"presunto"=>10.9,"mortadela"=>"4.3");
-    //laticinios=array("queijo"=>"10.0","presunto"=>"10.9","mortadela"=>"4.3");
-    print_r($laticinios);
-
+    //$laticinios=array(1 =>10.0,"presunto"=>10.9,"mortadela"=>4.3);
+    //$laticinios=array("queijo"=>"10.0","presunto"=>"10.9","mortadela"=>"4.3");
+    //print_r($laticinios);
 
     $jogo = array(
             array(1, "Zé", 11), 
@@ -24,8 +23,7 @@
 
      $mulheres = array(1 => array(1=>"ANA",2=>"BEATRIZ",3=>"PAULA"),
                        2 => array(1=>"ADRIANA",2=>"RAFAELA",3=>"NATALIA"),     
-                       3 => array(1=>"ERIKA",2=>"RAFA",3=>"POLIANA")
-    ); 
+                       3 => array(1=>"ERIKA",2=>"RAFA",3=>"POLIANA"); 
 
     for($i=1; $i<4;$i++){
         for($j=1; $j<4; $j++){
